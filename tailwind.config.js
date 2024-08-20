68e49acb52dc0df4eb7439da9edd5fd05a62fb60
daniel garcia <dany2017305@gmail.com>
2024-08-20 17:05:32 -0500

    versión movil

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,54 @@
 module.exports = {
   content: ['./**/*.html'],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'primary': '#CC2D4A',
+        'secondary': '#8FA206',
+        'tertiary': '#61AEC9'
+      },
+      backgroundImage:{
+        'sanFrancisco': "url('/public/img/sanFrancisco.jpg')",
+        'sanFranciscoDesktop': "url(/public/img/sanFranciscoDesktop.jpg')",
+			  'yosemite': "url('/public/img/yosemite,jpg')",
+			  'LA': "url('/public/img/LA.jpg')",
+			  'seattle': "url('/public/img/seattle.jpg')",
+			  'new_york': "url('/public/img/new_york.jpg')",
+			  'norway': "url('/public/img/norway.jpg')",
+			  'sydney': "url('/public/img/sydney.jpg')",
+			  'miami': "url('/public/img/miami.jpg')",
+			  'switzerland': "url('/public/img/switzerland.jpg')",
+			  'bali': "url('/public/img/bali.jpg')",
+			  'norway': "url('/public/img/norway.jpg')",
+			  'chicago': "url('/public/img/chicago.jpg')",
+			  'europe': "url('/public/img/europe.jpg')",
+			  'iceland': "url('/public/img/iceland.jpg')",
+      },
+      fontFamily:{
+        'Montserrat':['Montserrat', 'sans-serif']
+      },
+      // overscrollBehavior: {
+      //   'contain-x': 'contain',
+      // },
+      // scrollSnapType: {
+      //   'x-proximity': 'x proximity',
+      // },
+    },
   },
-  plugins: [],
+  plugins: [
+    require('tailwind-scrollbar-hide'),
+    function({ addUtilities }) {
+      const newUtilities = {
+        '.scroll-snap-x-proximity': {
+          'scroll-snap-type': 'x proximity',
+        },
+        '.scroll-snap-align-center': {
+          'scroll-snap-align': 'center',
+        },
+      }
+
+      addUtilities(newUtilities, ['responsive', 'hover', 'focus']);
+    },
+  ],
 }
 
